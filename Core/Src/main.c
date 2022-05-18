c72ec0f2216506f3925f796efad45eee1d5b4ea8
gcasti <gcasti_17@yahoo.com.ar>
2022-05-18 11:29:07 -0300

    Project Setup

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,88 +1,469 @@
+/* USER CODE BEGIN Header */
 /**
- * Copyright (c) May 6, 2022 Lautaro Vera <lautarovera93@gmail>.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- *
- * @file    : main.c
- * @date    : May 6, 2022
- * @author  : Lautaro Vera <lautarovera93@gmail.com>
- * @version : v1.0.0
- */
-
-/********************** inclusions *******************************************/
-
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
 #include "cmsis_os.h"
 
-/********************** macros and definitions *******************************/
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+/* Standard includes. */
+#include <stdio.h>
+#include <stdlib.h>
 
-/********************** internal data declaration ****************************/
+/* Demo includes. */
+#include "supportingFunctions.h"
 
-/********************** internal functions declaration ***********************/
+/* USER CODE END Includes */
 
-/********************** internal data definition *****************************/
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
 
-osThreadId defaultTaskHandle;
+/* USER CODE END PTD */
 
-/********************** external data definition *****************************/
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
 
-extern void mcu_init(void);
-extern void app(void);
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+/* Define the strings that will be passed in as the task parameters.  These are
+defined const and off the stack to ensure they remain valid when the tasks are
+executing. */
+const char *pcTextForTaskStats = "Task Stats  is running\r\n";
+const char *pcTextForTaskDemo1 = "Task Demo 1 is running\r\n";
+const char *pcTextForTaskDemo2 = "Task Demo 2 is running\r\n";
 
-/********************** internal functions definition ************************/
+/* USER CODE END PM */
 
-static void task_default(void const *argument)
+/* Private variables ---------------------------------------------------------*/
+ TIM_HandleTypeDef htim2;
+
+UART_HandleTypeDef huart3;
+
+osThreadId TaskStatsHandle;
+osThreadId TaskDemo1Handle;
+osThreadId TaskDemo2Handle;
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART3_UART_Init(void);
+static void MX_TIM2_Init(void);
+void vTaskStats_(void const * argument);
+void vTaskDemo_(void const * argument);
+
+/* USER CODE BEGIN PFP */
+#if( TASKS_SCOPE == TASKS_OUTSIDE_MAIN)
+extern void vTaskStats(void const * argument);
+extern void vTaskDemo(void const * argument);
+extern volatile unsigned long ulHighFrequencyTimerTicks;
+#endif
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
 {
-  for (;;)
+  /* USER CODE BEGIN 1 */
+	const char *pcTextForMain = "freertos_book_Example1_6 is running: Print run time statistics and task list\r\n\n";
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART3_UART_Init();
+  MX_TIM2_Init();
+  /* USER CODE BEGIN 2 */
+    vPrintString( pcTextForMain );
+
+#if( TASKS_SCOPE == TASKS_OUTSIDE_MAIN)
+    /* Start timer */
+    HAL_TIM_Base_Start_IT(&htim2);
+
+    /* Create the thread(s) */
+	/* definition and creation of TaskStats */
+    osThreadDef( TaskStats, vTaskStats, osPriorityAboveNormal, 0, 128+32 );
+    TaskStatsHandle = osThreadCreate( osThread(TaskStats), (void*)pcTextForTaskStats );
+
+    /* Check the task was created successfully */
+    configASSERT( TaskStatsHandle != NULL );
+
+    /* definition and creation of DemoTask1 */
+    osThreadDef( TaskDemo1, vTaskDemo, osPriorityNormal, 0, 128 );
+    TaskDemo1Handle = osThreadCreate( osThread(TaskDemo1), (void*)pcTextForTaskDemo1 );
+
+    /* Check the task was created successfully */
+    configASSERT( TaskDemo1Handle != NULL );
+
+    /* definition and creation of DemoTask2 */
+    osThreadDef( TaskDemo2, vTaskDemo, osPriorityNormal, 0, 128 );
+    TaskDemo2Handle = osThreadCreate( osThread(TaskDemo2), (void*)pcTextForTaskDemo2 );
+
+    /* Check the task was created successfully */
+    configASSERT( TaskDemo2Handle != NULL );
+#endif
+
+  /* USER CODE END 2 */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+#if( TASKS_SCOPE == TASKS_INSIDE_MAIN)
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* definition and creation of TaskStats */
+  osThreadDef(TaskStats, vTaskStats_, osPriorityAboveNormal, 0, 160);
+  TaskStatsHandle = osThreadCreate(osThread(TaskStats), (void*)pcTextForTaskStats);
+
+  /* definition and creation of TaskDemo1 */
+  osThreadDef(TaskDemo1, vTaskDemo_, osPriorityNormal, 0, 128);
+  TaskDemo1Handle = osThreadCreate(osThread(TaskDemo1), (void*)pcTextForTaskDemo1);
+
+  /* definition and creation of TaskDemo2 */
+  osThreadDef(TaskDemo2, vTaskDemo_, osPriorityNormal, 0, 128);
+  TaskDemo2Handle = osThreadCreate(osThread(TaskDemo2), (void*)pcTextForTaskDemo2);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+#endif
+  /* USER CODE END RTOS_THREADS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
   {
-    osDelay(1);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
   }
 }
 
-/********************** external functions definition ************************/
-
-int main(void)
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
 {
-  osThreadDef(defaultTask, task_default, osPriorityNormal, 0, 128);
-  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
-  mcu_init();
-  app();
+  /* USER CODE BEGIN TIM2_Init 0 */
 
-  osKernelStart();
+  /* USER CODE END TIM2_Init 0 */
 
-  /* Code should not reach this point.
-   * If this line executed is not enough heap space for default task.
-   */
-  configASSERT(0);
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 2-1;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 42-1;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
 }
-/********************** end of file ******************************************/
+
+/**
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 115200;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LD1_Pin|LD3_Pin|LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : USER_Btn_Pin */
+  GPIO_InitStruct.Pin = USER_Btn_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LD1_Pin LD3_Pin LD2_Pin */
+  GPIO_InitStruct.Pin = LD1_Pin|LD3_Pin|LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_vTaskStats_ */
+/**
+  * @brief  Function implementing the TaskStats thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_vTaskStats_ */
+void vTaskStats_(void const * argument)
+{
+  /* USER CODE BEGIN 5 */
+	/* The string to print out is passed in via the parameter.  Cast this to a
+	   character pointer. */
+	char *pcTaskName;
+	pcTaskName = ( char * ) argument;
+
+	/* Print out the name of this task. */
+	vPrintString( pcTaskName );
+
+	/* Infinite loop */
+	for(;;)
+	{
+	  osDelay(1);
+	}
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_vTaskDemo_ */
+/**
+* @brief Function implementing the TaskDemo1 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_vTaskDemo_ */
+void vTaskDemo_(void const * argument)
+{
+  /* USER CODE BEGIN vTaskDemo_ */
+	/* The string to print out is passed in via the parameter.  Cast this to a
+	   character pointer. */
+	char *pcTaskName;
+	pcTaskName = ( char * ) argument;
+
+	/* Print out the name of this task. */
+	vPrintString( pcTaskName );
+
+	/* Infinite loop */
+	for(;;)
+	{
+	  osDelay(1);
+	}
+  /* USER CODE END vTaskDemo_ */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM7 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+#if( TASKS_SCOPE == TASKS_OUTSIDE_MAIN)
+    if (htim->Instance == TIM2) {
+    	ulHighFrequencyTimerTicks++;
+	}
+#endif
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM7) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
